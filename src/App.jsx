4968da4e02a29d5ddc24775100d7728f64737e06
harshankumareshan <harshank333@gmail.com>
2023-11-15 12:52:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./components/Dashboard";
+
+
+
+
+function App() {
+ 
+
+  return <>
+      <div id="wrapper">
+      <Sidebar/>
+      <Dashboard/>
+      </div>
+      
+    
+      
+    </>
+  
+}
+
+export default App
